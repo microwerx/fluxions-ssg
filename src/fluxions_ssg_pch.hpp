bf07f21f2b00d5f3727564a4139f434650eff7dc
microwerx <jmetzgar@outlook.com>
2020-11-27 10:16:28 -0700

    Add files from fluxions project

---
+++ src/fluxions_ssg_pch.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cassert>
+
+#include <damselfish.hpp>
+#include <fluxions_base.hpp>
+#include <fluxions_ssg_base.hpp>
+#include <fluxions_ssg_ssphh.hpp>
+#include <fluxions_ssg_ssphh_renderer_plugin.hpp>
+#include <fluxions_ssg_ssphh_utilities.hpp>
